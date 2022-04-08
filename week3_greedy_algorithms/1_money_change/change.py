0c20a96675b264b17699e343724494826057de8d
Khaled Hagras <khaledhagras98@gmail.com>
2022-04-08 03:56:04 +0200

    Greedy Algorithms problems

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -1,11 +1,18 @@
-# Uses python3
-import sys
+#Problem Introduction#
+#In this problem, you will design and implement an elementary greedy algorithm used by cashiers all over the world millions of times per day.
 
 #Problem Description#
 #Task: The goal in this problem is to find the minimum number of coins needed to change the input value (an integer) into coins with denominations 1, 5, and 10.
 #Input Format: The input consists of a single integer 𝑚.
 #Constraints: 1 ≤ 𝑚 ≤ 103.
 #Output Format: Output the minimum number of coins with denominations 1, 5, 10 that changes 𝑚.
+
+
+
+
+# Uses python3
+import sys
+
 
 def get_change(m):
     n_coins = m // 10
--- week3_greedy_algorithms/7_maximum_salary/largest_number.py
+++ week3_greedy_algorithms/7_maximum_salary/largest_number.py
@@ -1,17 +1,36 @@
+#Problem Introduction#
+#Problem Introduction
+#As the last question of a successful interview, your boss gives you a few pieces of paper with numbers on it and asks you to compose a largest number from these numbers. The resulting number is going to be your salary, so you are very much interested in maximizing this number. How can you do this?
+
+#Problem Description#
+#Input Format: The first line of the input contains an integer 𝑛. The second line contains integers 𝑎1, 𝑎2, . . . , 𝑎𝑛.
+#Task: Compose the largest number out of a set of integers.
+#Constraints: 1 ≤ 𝑛 ≤ 100; 1 ≤ 𝑎𝑖 ≤ 103 for all 1 ≤ 𝑖 ≤ 𝑛.
+#Output Format: Output the largest number that can be composed out of 𝑎1, 𝑎2, . . . , 𝑎𝑛.
+
+
+
+
 #Uses python3
 
 import sys
 
+def larger_number(n,m):
+    if n+m >= m+n : return n
+    else: return m
+            
 def largest_number(a):
-    #write your code here
     res = ""
-    for x in a:
-        res += x
+    while len(a) > 1:
+        max_ = a[0]
+        for y in a[1:]: 
+            max_ = larger_number(max_,y)
+        res += max_
+        a.remove(max_)
+    res += a[0]
     return res
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = input.split()
-    a = data[1:]
-    print(largest_number(a))
-    
+    n = int(input())
+    a = list(input().split())
+    print(int(largest_number(a)))
